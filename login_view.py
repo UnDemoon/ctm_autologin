3421e598ff503c3659844e4622a4665259c44e6b
Demoon <liu_zun@foxmail.com>
2020-03-25 16:22:20 +0800

    新增配置文件

---
+++ login_view.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'login_view.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(287, 271)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.setObjectName("formLayout")
+        self.platformLabel = QtWidgets.QLabel(self.centralwidget)
+        self.platformLabel.setObjectName("platformLabel")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.platformLabel)
+        self.platformComboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.platformComboBox.setObjectName("platformComboBox")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.platformComboBox)
+        self.user_nameLabel = QtWidgets.QLabel(self.centralwidget)
+        self.user_nameLabel.setObjectName("user_nameLabel")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.user_nameLabel)
+        self.user_nameLineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.user_nameLineEdit.setObjectName("user_nameLineEdit")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.user_nameLineEdit)
+        self.user_pwdLabel = QtWidgets.QLabel(self.centralwidget)
+        self.user_pwdLabel.setObjectName("user_pwdLabel")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.user_pwdLabel)
+        self.user_pwdLineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.user_pwdLineEdit.setObjectName("user_pwdLineEdit")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.user_pwdLineEdit)
+        self.verticalLayout.addLayout(self.formLayout)
+        self.signin = QtWidgets.QPushButton(self.centralwidget)
+        self.signin.setObjectName("signin")
+        self.verticalLayout.addWidget(self.signin)
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setObjectName("textBrowser")
+        self.verticalLayout.addWidget(self.textBrowser)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 287, 23))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.platformLabel.setText(_translate("MainWindow", "平台"))
+        self.user_nameLabel.setText(_translate("MainWindow", "用户名"))
+        self.user_pwdLabel.setText(_translate("MainWindow", "密码"))
+        self.signin.setText(_translate("MainWindow", "Sign In"))
--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-from DecryptLogin import login
-import os
 import sys
+import os
+import json
+from PyQt5 import  QtCore, QtWidgets, uic
+#   引入ui文件
+from login_view import Ui_MainWindow as Ui
+#   引入登录模块
+from DecryptLogin import login
 
-if __name__ == '__main__':
-    script, user_account, user_pwd, *_ = sys.argv
-    lg = login.Login()
-    infos_return, session = lg.zhihu(user_account, user_pwd, 'pc')
-    print(infos_return, session)
+
+class MyApp(QtWidgets.QMainWindow, Ui):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui.__init__(self)
+        self.setupUi(self)
+        self._initdata();
+
+    def _initdata(self):
+        with open("./config.json", encoding='utf-8') as f:
+            config = json.load(f)
+            for k in config:
+                self.platformComboBox.addItem(config[k]["name"])
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
