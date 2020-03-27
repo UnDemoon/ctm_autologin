3ec330e6a5152bf3352b864f5b83ca0d9e9a5a66
Demoon <liu_zun@foxmail.com>
2020-03-27 16:31:06 +0800

    问题：网站验证机制障碍

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from PyQt5 import  QtCore, QtWidgets, uic
 from login_view import Ui_MainWindow as Ui
 #   引入登录模块
 from DecryptLogin import login
+from PIL import Image
 
+
+
+def fname(fuc):
+    return fuc
 
 class MyApp(QtWidgets.QMainWindow, Ui):
     def __init__(self):
@@ -14,12 +19,34 @@ class MyApp(QtWidgets.QMainWindow, Ui):
         Ui.__init__(self)
         self.setupUi(self)
         self._initdata();
+        self.signinButton.clicked.connect(self.signin)
 
     def _initdata(self):
-        with open("./config.json", encoding='utf-8') as f:
-            config = json.load(f)
-            for k in config:
-                self.platformComboBox.addItem(config[k]["name"])
+        with open("./config-default.json", encoding='utf-8') as defcfg:
+            cfg = json.load(defcfg)
+            for k in cfg:
+                self.platformComboBox.addItem(cfg[k]["name"], k)
+
+    def signin(self):
+        platform = self.platformComboBox.currentData()
+        usarname = self.user_nameLineEdit.text()
+        userpwd = self.user_pwdLineEdit.text()
+        lg = login.Login()
+        fuc = getattr(lg, platform, None)
+        infos_return, session = fuc(usarname, userpwd, 'pc', app = self)
+        print(infos_return, session)
+
+    def userInput(self):
+        code, *_ = QtWidgets.QInputDialog.getText(self, "验证码", "请输入验证码:", QtWidgets.QLineEdit.Normal, '')
+        return code
+
+    def crackvcFunc(self, imagepath):
+        # 打开验证码图片
+        img = Image.open(imagepath)
+        # 识别验证码图片
+        result = Image.IdentifyAPI(img)
+        # 返回识别结果(知乎为数字验证码)
+        return result
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
